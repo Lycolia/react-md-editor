8b2114aee8e01af21a398a3c6170b8d2134c291e
Lycolia Rizzim <lycolia@lycolia.info>
2021-02-16 23:57:17 +0900

    add sample, revert app

--- src/components/SimpleEditor.tsx
+++ src/components/SimpleEditor.tsx
@@ -1,7 +1,7 @@
 import { Editor, EditorState } from 'draft-js';
 import { useState } from 'react';
 
-export const EditorSample = () => {
+export const SimpleEditor = () => {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import App from './App';
+import { SimpleEditor } from './components/SimpleEditor';
 
 export const routes = [
   {
     path: '/',
     exact: true,
     component: App,
+  },
+  {
+    path: '/simple_editor',
+    exact: true,
+    component: SimpleEditor,
   },
 ];
