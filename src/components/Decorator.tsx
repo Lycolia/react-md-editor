e67ff008389b94b7982e8705e2f10706a06eb897
Lycolia Rizzim <lycolia@lycolia.info>
2021-02-21 13:42:21 +0900

    fix decorator

--- src/components/Decorator.tsx
+++ src/components/Decorator.tsx
@@ -4,30 +4,33 @@ import { HeaderDecorator } from '../decorators/HeaderDecorator';
 
 const compositeDecorator = new CompositeDecorator(HeaderDecorator);
 
-const turnOffHandleDecorations = () => {
+export const handleDecorations = () => {
   return EditorState.set(EditorState.createEmpty(), {
     decorator: compositeDecorator,
   });
 };
 
-const useEditor = () => {
-  const [editorState, setEditorState] = useState(() =>
-    turnOffHandleDecorations()
-  );
+export const useEditor = () => {
+  const [editorState, setEditorState] = useState(() => handleDecorations());
+
+  const onChange = (ev: React.SetStateAction<EditorState>) => {
+    console.log(
+      editorState.getLastChangeType(),
+      editorState.getCurrentContent().getPlainText(),
+      editorState.getCurrentContent().getBlockMap(),
+      editorState.getCurrentContent()
+    );
+    setEditorState(ev);
+  };
 
   return {
     editorState,
-    onChange: setEditorState,
+    onChange,
   };
 };
 
 export const Decorator = () => {
   const editor = useEditor();
 
-  return (
-    <Editor
-      editorState={editor.editorState}
-      onChange={(ev) => editor.onChange(ev)}
-    />
-  );
+  return <Editor editorState={editor.editorState} onChange={editor.onChange} />;
 };
---
+++ src/decorators/HeaderDecorator.spec.tsx
@@ -0,0 +1,9 @@
+// import { useEditor } from '../components/Decorator';
+
+// const editor = useEditor();
+
+// editor.editorState.
+
+// describe('header1Strategy', () => {
+//   it('render', () => {});
+// });
