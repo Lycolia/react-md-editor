2baa488a4d2f8705302188aca42a18b55a8d7a7a
Lycolia Rizzim <lycolia@lycolia.info>
2021-02-21 19:10:43 +0900

    remove junk

--- src/decorators/HeaderDecorator.spec.tsx
+++
@@ -1,9 +0,0 @@
-// import { useEditor } from '../components/Decorator';
-
-// const editor = useEditor();
-
-// editor.editorState.
-
-// describe('header1Strategy', () => {
-//   it('render', () => {});
-// });
