acca8863b340db6c0e4fbd04827e04327515c59a
Lycolia Rizzim <lycolia@lycolia.info>
2021-02-21 19:13:44 +0900

    remove junks

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,4 @@
 import App from './App';
-import { Decorator } from './components/Decorator';
-import { EditorBasic } from './components/EditorBasic';
-import { GetCaret } from './components/GetCarret';
-import { KeyCommand } from './components/KeyCommand';
 import { ToggleBlock } from './components/ToggleBlock';
 
 export const routes = [
@@ -10,26 +6,6 @@ export const routes = [
     path: '/',
     exact: true,
     component: App,
-  },
-  {
-    path: '/basic',
-    exact: true,
-    component: EditorBasic,
-  },
-  {
-    path: '/keyinput',
-    exact: true,
-    component: KeyCommand,
-  },
-  {
-    path: '/getcaret',
-    exact: true,
-    component: GetCaret,
-  },
-  {
-    path: '/decorator',
-    exact: true,
-    component: Decorator,
   },
   {
     path: '/toggleblock',
