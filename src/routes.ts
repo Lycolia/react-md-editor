0dbc2b4c904cb773dfbf45c5ff9204467ee64306
Lycolia Rizzim <lycolia@lycolia.info>
2021-02-23 12:11:57 +0900

    fix routing

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,9 @@
 import App from './App';
-import { ToggleBlock } from './components/ToggleBlock';
 
 export const routes = [
   {
     path: '/',
     exact: true,
     component: App,
-  },
-  {
-    path: '/toggleblock',
-    exact: true,
-    component: ToggleBlock,
   },
 ];
